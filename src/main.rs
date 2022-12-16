d6c4ac06da8ac3c95ef5690be1edfca2acfb4bd2
Yuval Dolev <yuvaldolev2@gmail.com>
2022-12-17 01:51:30 +0200

    feat: add command line argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(author = "ydolev", version = "1.0.0", about = "A pubsub system written in Rust", long_about = None)]
+struct Cli {
+    #[arg(long)]
+    pub_port: u16,
+
+    #[arg(long)]
+    sub_port: u16,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _cli = Cli::parse();
 }
